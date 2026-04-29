89263685363202aa6987e0d2cf027dec40b7b7bc
Macch1 <awm18@live.dk>
2026-04-29 10:53:29 +0200

    First Update: Setup of Maven.
    First Update: Setup of Maven.

---
+++ Core/src/main/java/dk/sdu/se4/labproject/core/App.java
@@ -0,0 +1,13 @@
+package dk.sdu.se4.labproject.core;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
+}
---
+++ Core/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module Core {
+
+    // module-info.java:
+    // Correctly declare module dependencies and exported packages
+
+
+
+    // requires: Use to declare dependencies.
+    // requires _____;
+
+
+
+
+    // exports: Use to make packages accessible.
+    //
+
+
+
+}
