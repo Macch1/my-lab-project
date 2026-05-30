b7f790b741fabb15fc1aa22c307e84e9edcae89e
Macch1 <awm18@live.dk>
2026-05-30 12:47:30 +0200

    tried to fix naming and packages.

--- EnemyShip/src/main/java/module-info.java
+++ EnemyShip/src/main/java/module-info.java
@@ -1,5 +1,9 @@
-
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 
 module EnemyShip {
     requires Common;
+    requires CommonEnemy;
+    provides IGamePluginService with dk.sdu.se4.groupX.enemyship.EnemyShipPlugin;
+    provides IEntityProcessingService with dk.sdu.se4.groupX.enemyship.EnemyShipProcessingSystem;
 }
