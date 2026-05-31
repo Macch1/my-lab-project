296b6fae59c80037536de5cf6995383dd6a7545c
Macch1 <awm18@live.dk>
2026-05-31 12:28:00 +0200

    Testing Collision.

---
+++ Collision/src/test/java/dk/sdu/mmmi/cbse/collisionsystem/CollisionDetectorTest.java
@@ -0,0 +1,35 @@
+package dk.sdu.mmmi.cbse.collisionsystem;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CollisionDetectorTest {
+
+    private CollisionDetector collisionDetector;
+    private Entity entity1;
+    private Entity entity2;
+
+    @BeforeEach
+    void setUp() {
+        collisionDetector = new CollisionDetector();
+        entity1 = new Entity();
+        entity2 = new Entity();
+    }
+
+    @Test
+    void testCollides_WhenOverlapping_ReturnsTrue() { ... }
+
+    @Test
+    void testCollides_WhenApart_ReturnsFalse() { ... }
+
+    @Test
+    void testCollides_WhenTouching_ReturnsFalse() { ... }
+
+    @Test
+    void testResolveCollision_WhenCannotCollide_NoDamage() { ... }
+
+    @Test
+    void testResolveCollision_WhenCanDamage_DamageApplied() { ... }
+}
