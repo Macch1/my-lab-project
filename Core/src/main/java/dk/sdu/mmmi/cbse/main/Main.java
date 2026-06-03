33ecc57ed59c26ea8fdd53605ad3ebd977bdba61
Macch1 <awm18@live.dk>
2026-06-03 15:15:49 +0200

    Fixing Score.

--- Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -17,22 +17,6 @@ public class Main extends Application {
     public void start(Stage window) throws Exception
     {
 
-        // Start the HighScoreSystem as a separate process before the game starts.
-        try
-        {
-            new ProcessBuilder("java", "-jar", "HighScoreSystem/target/HighScoreSystem-0.0.1-SNAPSHOT.jar").inheritIO().start();
-
-            // Give HighScoreSystem time to start up before the game begins.
-            Thread.sleep(1500);
-
-            System.out.println("HighScoreSystem started.");
-        }
-        catch (Exception e)
-        {
-            // Game continues even if HighScoreSystem fails to start.
-            System.out.println("HighScoreSystem could not be started: " + e.getMessage());
-        }
-
         // .
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ModuleConfig.class);
 
